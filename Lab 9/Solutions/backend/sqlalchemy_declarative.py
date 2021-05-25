8aca74e0f22281dedd4c6b3aeac61972a5871985
Łukasz Szczęśniak <kontakt@lukaszszczesniak.com>
2021-05-25 12:28:41 +0200

    Add SQLAlchemy declarative db and db creation script

---
+++ Lab 9/Solutions/backend/sqlalchemy_declarative.py
@@ -0,0 +1,40 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'Users'
+    id = Column(Integer, primary_key=True)
+    login = Column(String(250), unique=True, nullable=False)
+    password = Column(String(250), nullable=False)
+
+class PrivateMessage(Base):
+    __tablename__ = 'PrivateMessages'
+    id = Column(Integer, primary_key=True)
+    senderId = Column(Integer, ForeignKey('Users.id'), nullable=False)
+    recipientId = Column(Integer, ForeignKey('Users.id'), nullable=False)
+    user = relationship(User)
+    sendTime = Column(DateTime)
+    readStatus = Column(Boolean, nullable=False, default=0)
+
+class BroadcastMessage(Base):
+    __tablename__ = 'BroadcastMessages'
+    id = Column(Integer, primary_key=True)
+    senderId = Column(Integer, ForeignKey('Users.id'), nullable=False)
+    user = relationship(User)
+    sendTime = Column(DateTime)
+
+class BroadcastMsgStatus(Base):
+    __tablename__ = 'BroadcastMsgStatuses'
+    broadcastmsgId = Column(Integer, ForeignKey('BroadcastMessages.id'), primary_key=True, nullable=False)
+    message = relationship(BroadcastMessage)
+    readerId = Column(Integer, ForeignKey('Users.id'), primary_key=True, nullable=False)
+    user = relationship(User)
+
+engine = create_engine('sqlite:///messenger.db')
+Base.metadata.create_all(engine)
